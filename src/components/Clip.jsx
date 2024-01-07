7256d84dab518291e4c70bd60a63e3eb2a810154
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-07 23:19:21 +0500

    open component in modal

--- src/components/Clip.jsx
+++ src/components/Clip.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { FaCopy, FaPaste } from 'react-icons/fa';
 import './Clip.css';
 
 const ClipboardExample = () => {
 	const [copiedText, setCopiedText] = useState('');
 	const [title, setTitle] = useState('');
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const handleCopyToClipboard = async () => {
 		try {
@@ -28,31 +30,41 @@ const ClipboardExample = () => {
 		}
 	};
 
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
+
 	return (
 		<div className="relative flex flex-col items-center justify-center h-screen">
-			<div className="relative w-1/2 h-1/2 mb-2">
-				<input
-					type="text"
-					placeholder="Enter the title"
-					className="bg-slate-200 text-black font-semibold border border-gray-500 rounded-lg p-2 w-full mb-10"
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
-				/>
-				<div className="absolute top-0 right-0 mt-16 mr-2 flex items-center">
-					<button className="mr-2" onClick={handleCopyToClipboard}>
-						<FaCopy  title='Copy'/>
-					</button>
-					<button onClick={handlePasteFromClipboard}>
-						<FaPaste  title='Paste'/>
-					</button>
+			<button onClick={openModal}>Open Modal</button>
+			<Modal
+				isOpen={isModalOpen}
+				onRequestClose={closeModal}
+				contentLabel="Clipboard Modal"
+			>
+				<div className="relative w-1/2 h-1/2 mb-2">
+					<input
+						type="text"
+						placeholder="Enter the title"
+						className="bg-slate-200 text-black font-semibold border border-gray-500 rounded-lg p-2 w-full mb-10"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+					<div className="absolute top-0 right-0 mt-16 mr-2 flex items-center">
+						<button className="mr-2" onClick={handleCopyToClipboard}>
+							<FaCopy title="Copy" />
+						</button>
+						<button onClick={handlePasteFromClipboard}>
+							<FaPaste title="Paste" />
+						</button>
+					</div>
+					<textarea
+						className="bg-slate-200 text-black font-semibold border border-gray-500 rounded-lg p-2 w-full h-full mt-2 scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-200"
+						placeholder="Enter the text"
+						value={copiedText}
+						onChange={(e) => setCopiedText(e.target.value)}
+					/>
 				</div>
-				<textarea
-					className="bg-slate-200 text-black font-semibold border border-gray-500 rounded-lg p-2 w-full h-full mt-2 scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-200"
-					placeholder="Enter the text"
-					value={copiedText}
-					onChange={(e) => setCopiedText(e.target.value)}
-				/>
-			</div>
+			</Modal>
 		</div>
 	);
 };
