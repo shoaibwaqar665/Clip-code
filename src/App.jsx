66d8ab2432ed6cc4d85397e929cb4512be529d24
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-07 22:54:12 +0500

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import './App.css'
+import ClipboardExample from './components/Clip'
+import { FaTwitter, FaGithub, FaReact } from 'react-icons/fa';
+
+
+function App() {
+
+   return (
+      <>
+         <ClipboardExample />
+        
+      </>
+   )
+}
+
+export default App
