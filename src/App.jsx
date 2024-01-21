6560f7fffe917f2e610cc7f44ef6d25375587e62
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-22 00:00:30 +0500

    qc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 import Clip from './components/Refactor';
-import SumUp from './components/SumUp';
 
 
 function App() {
 
    return (
       <>
-         {/* <ClipboardExample /> */}
-         {/* <Clip/> */}
-         <SumUp/>
+         <Clip/>
         
       </>
    )
