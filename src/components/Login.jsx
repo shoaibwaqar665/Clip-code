7b38c09bf2531cc2925202861c36cfba75c32d59
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-28 17:09:11 +0500

    login File

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,57 +1,61 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { auth } from '../FirebaseConfig'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-function Login() {
-
+const Login = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-
 	const [loginError, setLoginError] = useState('');
 
-	const handleLogin = (e) => {
+	const handleLogin = async (e) => {
 		e.preventDefault();
-		auth.signInWithEmailAndPassword(email, password).then(() => {
+		const auth = getAuth();
+
+		try {
+			await signInWithEmailAndPassword(auth, email, password);
 			setEmail('');
 			setPassword('');
 			setLoginError('');
+			// Redirect to another page if needed
 			// props.history.push('/');
-		}).catch(err => setLoginError(err.message))
-	}
+		} catch (error) {
+			setLoginError(error.message);
+		}
+	};
 
 	return (
-		<div className='container'>
-			<br></br>
-			<br></br>
-			<h2>LOGIN HERE</h2>
-			<br></br>
-			<form autoComplete="off" className='form-group'
-				onSubmit={handleLogin}>
-
-				<label>Enter Email</label>
-				<input type="email" className='form-control'
-					required onChange={(e) => setEmail(e.target.value)}
+		<div className="container mx-auto my-8 p-4 max-w-md bg-white shadow-md">
+			<h2 className="text-2xl font-semibold mb-4">LOGIN HERE</h2>
+			<form autoComplete="off" className="form-group" onSubmit={handleLogin}>
+				<label className="block mb-2 text-gray-600">Enter Email</label>
+				<input
+					type="email"
+					className="form-control mb-2 p-2 border rounded"
+					required
+					onChange={(e) => setEmail(e.target.value)}
 					value={email}
 				/>
-				<br></br>
-				<label>Enter Password</label>
-				<input type="password" className='form-control'
-					required onChange={(e) => setPassword(e.target.value)}
+
+				<label className="block mb-2 text-gray-600">Enter Password</label>
+				<input
+					type="password"
+					className="form-control mb-4 p-2 border rounded"
+					required
+					onChange={(e) => setPassword(e.target.value)}
 					value={password}
 				/>
-				<br></br>
-				<button type="submit" className='btn btn-success mybtn2'>
-					LOGIN
+
+				<button type="submit" className="btn btn-success mybtn2 px-4 py-2 bg-green-500 text-white">
+					<Link to="/todo" >LOGIN</Link>
 				</button>
 			</form>
-			{loginError && <div className='error-msg'>
-				{loginError}
-			</div>}
+			{loginError && <div className="error-msg mt-4 text-red-500">{loginError}</div>}
 
-			<span>Don't have an account? Create One
-				<Link to="signup"> here</Link></span>
+			<span className="mt-4 block text-gray-600">
+				Don't have an account? Register <Link to="/signup" className="text-blue-500">here</Link>
+			</span>
 		</div>
-	)
-}
+	);
+};
 
 export default Login;
