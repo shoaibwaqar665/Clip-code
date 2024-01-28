cc58fd152245b59b83b189edf26af198835cb903
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-28 15:35:42 +0500

    add routing

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const SignUp = () => {
+	return (
+		<div>
+			<Link to="/todo">
+				<button>
+					todo
+				</button>
+			</Link>
+		</div>
+	)
+}
+
+export default SignUp
