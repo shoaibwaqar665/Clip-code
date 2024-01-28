1a3a55e43385d84ceb8cc12e0af9421393c6929d
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-28 16:26:07 +0500

    login modification

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../FirebaseConfig'
 
-const Login = () => {
+function Login() {
+
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const [loginError, setLoginError] = useState('');
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+		auth.signInWithEmailAndPassword(email, password).then(() => {
+			setEmail('');
+			setPassword('');
+			setLoginError('');
+			// props.history.push('/');
+		}).catch(err => setLoginError(err.message))
+	}
+
 	return (
-		<div>
-			<Link to="/signup">
-				<button>
-					SignUp
+		<div className='container'>
+			<br></br>
+			<br></br>
+			<h2>LOGIN HERE</h2>
+			<br></br>
+			<form autoComplete="off" className='form-group'
+				onSubmit={handleLogin}>
+
+				<label>Enter Email</label>
+				<input type="email" className='form-control'
+					required onChange={(e) => setEmail(e.target.value)}
+					value={email}
+				/>
+				<br></br>
+				<label>Enter Password</label>
+				<input type="password" className='form-control'
+					required onChange={(e) => setPassword(e.target.value)}
+					value={password}
+				/>
+				<br></br>
+				<button type="submit" className='btn btn-success mybtn2'>
+					LOGIN
 				</button>
-			</Link>
+			</form>
+			{loginError && <div className='error-msg'>
+				{loginError}
+			</div>}
+
+			<span>Don't have an account? Create One
+				<Link to="signup"> here</Link></span>
 		</div>
 	)
 }
 
-export default Login
+export default Login;
