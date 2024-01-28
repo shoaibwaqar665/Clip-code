e2fb8bdd2838d58b299a138d589cbbb85bb24684
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-01-28 15:35:15 +0500

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
+import Login from './components/Login';
 import Clip from './components/Refactor';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SignUp from './components/SignUp';
 
 function App() {
 
    return (
       <>
-         <Clip/>
-        
+         {/* <Clip/> */}
+         <Routes>
+            <Route path="/" element={< Login />} />
+            <Route path="/todo" element={< Clip />} />
+            <Route path="/signup" element={< SignUp />} />
+           
+         </Routes>
       </>
    )
 }
